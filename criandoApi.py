b874eec49921193c6594d0babcafb3bb2c238895
Gustavo H <88460364+kloin0@users.noreply.github.com>
2022-10-23 22:45:48 -0300

    Delete criandoApi.py

--- criandoApi.py
+++
@@ -1,9 +0,0 @@
-import requests 
-import json 
-# não vai funcionar pq o site ta fora do ar !!!!!!!
-link = 'https://api.kloin.repl.co/pagarVendas'
-requisicao = requests.get(link)
-#print(requisicao.json())
-dicionario = requisicao.json()
-print(dicionario['totalDeVendas'])
-
