6ff19e77cd1174fdbbba6c68f93f6b1a8097702b
Gustavo H <88460364+kloin0@users.noreply.github.com>
2022-10-23 22:45:06 -0300

    Delete espiralDeArquimedes.py

--- espiralDeArquimedes.py
+++
@@ -1,9 +0,0 @@
-import turtle
-import math 
-tela = turtle.Screen() 
-for c in range(1300):
-	vt = c / 40 * math.pi 
-	y = (vt * 5 + 5 ) * math.sin(vt)
-	x = (vt * 5 + 5 ) * math.cos(vt)
-	turtle.goto(x,y)
-tela.exitonclick()
